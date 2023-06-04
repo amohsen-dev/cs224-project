974137a5d8a7beb24f3ffdb512faafec1ef62f41
ali mohsen <al.moh302@gmail.com>
2023-06-04 03:55:47 -0400

    typo

--- src/play_against_opponent_RL.py
+++ src/play_against_opponent_RL.py
@@ -2,8 +2,8 @@ from self_play import ConsoleAgent, PNNAgent, play_random_game
 
 if __name__ == '__main__':
     agent1 = PNNAgent(
-        path_enn='../model_cache/RL/PG/model_enn_768.data',
-        path_pnn='../model_cache/RL/PG/model_pnn_768.data',
+        path_enn='../model_cache/RL/PG/model_enn_1536.data',
+        path_pnn='../model_cache/RL/PG/model_pnn_1536.data',
     )
     agent2 = ConsoleAgent()
     play_random_game(agent1, agent2, verbose=True)
