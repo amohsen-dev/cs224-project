482f3081cabd93455a481ac2559d06634e1049cc
Ali Mohsen <al.moh302@gmail.com>
2023-06-04 13:13:47 -0400

    mod

--- src/play_against_opponent_RL.py
+++ src/play_against_opponent_RL.py
@@ -2,8 +2,8 @@ from self_play import ConsoleAgent, PNNAgent, play_random_game
 
 if __name__ == '__main__':
     agent1 = PNNAgent(
-        path_enn='../model_cache/RL/PG/model_enn_2560.data',
-        path_pnn='../model_cache/RL/PG/model_pnn_2560.data',
+        path_enn='../model_cache/RL/PG/model_enn_31232.data',
+        path_pnn='../model_cache/RL/PG/model_pnn_31232.data',
     )
     agent2 = ConsoleAgent()
     play_random_game(agent1, agent2, verbose=True)
