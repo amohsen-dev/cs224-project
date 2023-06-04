7c07606ae863feed96ca6f5be52a0a9bbec74352
ali mohsen <al.moh302@gmail.com>
2023-06-04 13:29:35 -0400

    typo

--- src/self_play_RL_vs_SL.py
+++ src/self_play_RL_vs_SL.py
@@ -1,23 +1,27 @@
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from self_play import ConsoleAgent, PNNAgent, play_random_game
 
 if __name__ == '__main__':
-    agent1 = PNNAgent(
-        path_enn='../model_cache/RL/PG/model_enn_31232.data',
-        path_pnn='../model_cache/RL/PG/model_pnn_31232.data',
-    )
-    agent2 = PNNAgent(
-        path_enn='../model_cache/RL/PG/model_enn_0.data',
-        path_pnn='../model_cache/RL/PG/model_pnn_0.data',
-    )
+    comparison = pd.DataFrame(columns=['dIMP'])
+    for i in tqdm(range(0, 31744, 512)):
 
-    imps = []
-    for i in tqdm(range(64)):
-        path = play_random_game(agent1, agent2, verbose=False)
-        imp = path['rewards'][-1]
-        imps.append(imp)
+        agent1 = PNNAgent(
+            path_enn=f'../model_cache/RL/PG/model_enn_{i}.data',
+            path_pnn=f'../model_cache/RL/PG/model_pnn_{i}.data',
+        )
+        agent2 = PNNAgent(
+            path_enn='../model_cache/RL/PG/model_enn_0.data',
+            path_pnn='../model_cache/RL/PG/model_pnn_0.data',
+        )
 
-    print(pd.Series(imps).to_frame('IMP').expanding().std())
-    print(pd.Series(imps).to_frame('IMP').expanding().mean())
+        imps = []
+        for i in range(128):
+            path = play_random_game(agent1, agent2, verbose=False)
+            imp = path['rewards'][-1]
+            imps.append(imp)
+        comparison.loc[i] = np.mean(imps)
+        comparison.to_pickle('SL_vs_RL_comparison.pkl')
+        print(comparison)
 
