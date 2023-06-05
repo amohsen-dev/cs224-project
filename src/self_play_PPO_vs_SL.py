5ef6549b8e73d64bd44d7d6aa3f4e4cc5164767f
amohsen-dev <82631280+amohsen-dev@users.noreply.github.com>
2023-06-05 07:45:49 -0400

    Update self_play_PPO_vs_SL.py

--- src/self_play_PPO_vs_SL.py
+++ src/self_play_PPO_vs_SL.py
@@ -5,26 +5,28 @@ from self_play import ConsoleAgent, PNNAgent, play_random_game
 
 if __name__ == '__main__':
     comparison = pd.DataFrame(columns=['dIMP', 'nSample', 'stdIMP'])
-    for i in tqdm(range(0, 31744, 512)):
+    for i in tqdm(range(0, 31744, 128)):
+        try:
+            agent1 = PNNAgent(
+                path_enn=f'../model_cache/RL/PPO/model_enn_{i}.data',
+                path_pnn=f'../model_cache/RL/PPO/model_pnn_{i}.data',
+            )
+            agent2 = PNNAgent(
+                path_enn='../model_cache/RL/PPO/model_enn_0.data',
+                path_pnn='../model_cache/RL/PPO/model_pnn_0.data',
+            )
 
-        agent1 = PNNAgent(
-            path_enn=f'../model_cache/RL/PPO/model_enn_{i}.data',
-            path_pnn=f'../model_cache/RL/PPO/model_pnn_{i}.data',
-        )
-        agent2 = PNNAgent(
-            path_enn='../model_cache/RL/PPO/model_enn_0.data',
-            path_pnn='../model_cache/RL/PPO/model_pnn_0.data',
-        )
-
-        imps = []
-        for _ in range(128):
-            try:
-                path = play_random_game(agent1, agent2, verbose=False)
-                imp = path['rewards'][-1]
-                imps.append(imp)
-            except Exception:
-                pass
-        comparison.loc[i] = (np.mean(imps), len(imps), np.std(imps))
-        comparison.to_pickle('SL_vs_PPO_comparison.pkl')
-        print(comparison)
+            imps = []
+            for _ in range(128):
+                try:
+                    path = play_random_game(agent1, agent2, verbose=False)
+                    imp = path['rewards'][-1]
+                    imps.append(imp)
+                except Exception:
+                    pass
+            comparison.loc[i] = (np.mean(imps), len(imps), np.std(imps))
+            comparison.to_pickle('SL_vs_PPO_comparison.pkl')
+            print(comparison)
+        except Exception:
+            pass
 
